630c31039fcea720f81bb8c8ac4bc408e92191c9
Strakhov Mark <easytomalbolge@gmail.com>
2020-11-14 10:13:32 +0300

    DCGAN template code

---
+++ dcgan/discriminator.py
@@ -0,0 +1,6 @@
+"""Home for discriminator code."""
+from torch import nn
+
+
+class Discriminator(nn.Module):
+    """DCGAN discriminator."""
---
+++ dcgan/generator.py
@@ -0,0 +1,6 @@
+"""Home for generator code."""
+from torch import nn
+
+
+class Generator(nn.Module):
+    """DCGAN generator with Transposed convolutions."""
